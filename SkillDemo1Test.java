14acc80187a4c2dba78650ddb5e0ad46470a6357
janayagarcia <janayagarcia@ucsd.edu>
2022-04-30 16:48:26 -0700

    Update SkillDemo1Test.java

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test
     public void addition() {
-        assertEquals(5, SkillDemo1.multiply(2, 3));
+        assertEquals(6, SkillDemo1.multiply(2, 3));
     }
 }
